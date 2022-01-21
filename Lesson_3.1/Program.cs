78346c49e1aec7818b87e620e50c5632d3b2b34e
Eugene <comradeeugene@gmail.com>
2022-01-21 19:51:44 +0300

    finish

--- Lesson_3.1/Program.cs
+++ Lesson_3.1/Program.cs
@@ -6,7 +6,18 @@ namespace Lesson_3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите целое число");
+            int CheckValue = int.Parse(Console.ReadLine());
+
+            if (CheckValue % 2 == 0)
+            {
+                Console.WriteLine($"{CheckValue} - четное число");
+            }
+            else
+            {
+                Console.WriteLine($"{CheckValue} - не четное число");
+            }
+            Console.ReadKey();
         }
     }
 }
